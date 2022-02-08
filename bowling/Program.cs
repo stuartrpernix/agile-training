b26e58bc62d6f7e163579d7cb3dfe9fbf932d41c
William Ritossa <wjritossa@gmail.com>
2022-02-08 11:38:57 +1030

    Reads in player names and begines frames

--- bowling/Program.cs
+++ bowling/Program.cs
@@ -13,15 +13,27 @@ namespace ConsoleApp1
             // Initial Setup
             Console.WriteLine("Welcome to bowling. Enter the number of players:");
             int numPlayers = Convert.ToInt32(Console.ReadLine());
-            string[] players;
+            string[] players = new string[numPlayers];
             
             for (int i = 0; i < numPlayers; i++)
             {
-                Console.WriteLine("Enter the name of player");
+                Console.WriteLine("Enter the name of player {0}:", i+1);
+                players[i] = Console.ReadLine();
             }
 
             // Frames
             int numFrames = 10;
+
+            Console.WriteLine("It's time to start bowling!");
+            for (int i = 0; i < numFrames; i++)
+            {
+                Console.WriteLine($"Frame {i + 1}:");
+                for (int j = 0; i < numPlayers; i++)
+                {  
+                    Console.WriteLine("Delivery {0}", j+1);
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
